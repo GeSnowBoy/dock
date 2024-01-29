3b048b63666de0e95242187466f804bb19a6fde8
gebaoyuan <yuansnowboy@163.com>
2024-01-29 20:03:11 +0800

    init: 初始化

---
+++ src/.vuepress/config.js
@@ -0,0 +1,18 @@
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress/cli";
+import { viteBundler } from "@vuepress/bundler-vite";
+
+export default defineUserConfig({
+  lang: "en-US",
+
+  title: "文档中心",
+  description: "文档中心描述",
+
+  theme: defaultTheme({
+    logo: "https://vuejs.press/images/hero.png",
+
+    navbar: ["/", "/get-started"],
+  }),
+
+  bundler: viteBundler(),
+});
