dd7208bc8f465a5cdc2941157a8adafa9e54a2bb
gebaoyuan <yuansnowboy@163.com>
2024-02-04 17:51:10 +0800

    feat: 修改

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,13 +4,11 @@ import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
   lang: "en-US",
-
   title: "文档中心",
   description: "文档中心描述",
-
+  base: "/doc/",
   theme: defaultTheme({
     logo: "https://vuejs.press/images/hero.png",
-
     navbar: ["/", "/get-started"],
   }),
 
